720fad3f0b9e210fa67b91935bae2673553f8025
Todd Treece <toddtreece@gmail.com>
2012-10-31 16:02:39 -0600

    ...forgot to add db.js :/

---
+++ db.js
@@ -0,0 +1,93 @@
+var util = require('util');
+var EventEmitter = require('events').EventEmitter;
+
+var DB = function() {
+
+  // =====================
+  // = Private Variables =
+  // =====================
+  var mongo               = require('mongodb'),
+      crypto              = require('crypto'),
+      config              = require('./config'),
+      server              = new mongo.Server(config.mongo.host, config.mongo.port),
+      connector           = new mongo.Db(config.mongo.db, server, {safe: true}),
+      db                  = false,
+      backers_collection  = false,
+      self                = this;
+
+
+  // ========
+  // = Init =
+  // ========
+  connector.open(function(err, d) {
+
+    if(err)
+      throw err;
+
+    db = d;
+
+    db.collection('backers', function(err, collection) {
+
+      if(err)
+        throw err;
+
+      backers_collection = collection;
+
+    });
+
+  });
+
+  // ==================
+  // = Public Methods =
+  // ==================
+  this.process_backers = function(backers) {
+
+    for(var i = 0; i < backers.length; i++) {
+      process_backer(backers[i]);
+    }
+
+  };
+
+  // ===================
+  // = Private Methods =
+  // ===================
+  var process_backer = function(backer) {
+
+    backers_collection.findOne({user: backer.user}, function(err, b) {
+
+      if(b == null)
+        add_backer(backer);
+
+    });
+
+  };
+
+  var add_backer = function(backer) {
+
+    backer.hash = create_hash(backer);
+    backer.created = new Date();
+
+    backers_collection.insert(backer, function(err) {
+      if(!err)
+        self.emit('added_backer', backer);
+    });
+
+  };
+
+  var create_hash = function(backer) {
+
+    var sha = crypto.createHash('sha1');
+        sha.update(backer.user);
+        sha.update(backer.name);
+        sha.update(config.salt);
+        sha.update(backer.date_backed);
+        sha.update(backer.pledge);
+
+    return sha.digest('hex');
+
+  };
+
+};
+
+util.inherits(DB, EventEmitter);
+module.exports = DB;
